60c01c62763269ca3b692cadc3fdc756a686cf94
KannanKvsp <kannanpalani69@gmail.com>
2022-04-07 14:41:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
+function Heading(){
+  const [header, setHeader] = useState('');
+  const [headerinput, setHeaderInput] = useState('');
+  const [notes, setNotes] = useState([]);
+  const [noteinput, setNoteInput] = useState('');
+
+  if (header) {
+     return(<>
+     <h3>{header}</h3>
+     <ol>{notes.map((item)=>{
+          return <li key={item.key}>{item.value}</li>
+        })}</ol>
+    <input placeholder='type your notes' type="text" value={noteinput} onChange={(e)=>{setNoteInput(e.target.value)}}/>
+    <button onClick={()=>{
+      setNotes([...notes, {key: notes.length, value: noteinput}]);
+      setNoteInput('');
+    }}>Add Note</button>
+    <br/>
+    <button onClick={()=>{
+      setHeader('');
+      setHeaderInput('');
+      setNotes([]);
+      setNoteInput('');
+    }
+    }>Remove all</button>
+     </>
+     )}
+  else{
+    return(
+      <>
+      <input type="text" placeholder='Enter the title' value={headerinput} onChange={(e)=>setHeaderInput(e.target.value)}/>
+      <button onClick={()=>setHeader(headerinput)}>OK</button>
+      </>
+    )}
+}
 function App() {
+  const [list, setList] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Java sample approach</h1>
+      {Heading()}
     </div>
   );
 }
